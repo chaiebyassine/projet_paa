4eb912f2dfbb85f9b1367d302081a86bf38337f3
yassine092002 <yassinechaieb6@gmail.com>
2026-04-11 22:39:52 +0200

    Implémentation complète des mouvements de la Tour avec gestion des obstacles et captures

--- main.cpp
+++ main.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <vector>
 #include "src/model/Plateau.h"
-#include "src/model/Tour.h"
+#include "src/model/Fou.h"
 #include "src/model/JoueurHumain.h"
-
+#include "src/model/Tour.h"
 int main() {
     Plateau plateau(8, 8);
 
     JoueurHumain j1("Joueur1", Couleur::BLANC);
     JoueurHumain j2("Joueur2", Couleur::NOIR);
 
-    Tour tourBlanche(Position(3, 3), Couleur::BLANC, &j1);
-    Tour tourNoire(Position(3, 6), Couleur::NOIR, &j2);
-    Tour tourAlliee(Position(1, 3), Couleur::BLANC, &j1);
+    Fou fouBlanc(Position(3, 3), Couleur::BLANC, &j1);
+    Tour pieceAdverse(Position(5, 5), Couleur::NOIR, &j2);
+    Tour pieceAlliee(Position(1, 1), Couleur::BLANC, &j1);
 
-    plateau.placerPiece(Position(3, 3), &tourBlanche);
-    plateau.placerPiece(Position(3, 6), &tourNoire);
-    plateau.placerPiece(Position(1, 3), &tourAlliee);
+    plateau.placerPiece(Position(3, 3), &fouBlanc);
+    plateau.placerPiece(Position(5, 5), &pieceAdverse);
+    plateau.placerPiece(Position(1, 1), &pieceAlliee);
 
-    std::vector<Position> mouvements = tourBlanche.mouvementsPossibles(plateau);
+    std::vector<Position> mouvements = fouBlanc.mouvementsPossibles(plateau);
 
-    std::cout << "Mouvements possibles de la tour blanche :" << std::endl;
+    std::cout << "Mouvements possibles du fou blanc :" << std::endl;
     for (const Position& p : mouvements) {
         std::cout << "(" << p.getLigne() << ", " << p.getColonne() << ")" << std::endl;
     }
---
+++ src/model/fou.cpp
@@ -0,0 +1,54 @@
+#include "Fou.h"
+#include "Plateau.h"
+#include "Case.h"
+#include "Piece.h"
+
+Fou::Fou(const Position& pos, Couleur coul, Joueur* j)
+    : Piece(pos, coul, j) {
+}
+
+std::vector<Position> Fou::mouvementsPossibles(const Plateau& plateau) const {
+    std::vector<Position> mouvements;
+
+    const int directions[4][2] = {
+        {-1, -1}, // haut-gauche
+        {-1, 1},  // haut-droite
+        {1, -1},  // bas-gauche
+        {1, 1}    // bas-droite
+    };
+
+    for (int d = 0; d < 4; ++d) {
+        int dl = directions[d][0];
+        int dc = directions[d][1];
+
+        int ligne = position.getLigne() + dl;
+        int colonne = position.getColonne() + dc;
+
+        while (true) {
+            Position p(ligne, colonne);
+
+            if (!plateau.estCaseValide(p)) {
+                break;
+            }
+
+            const Case* c = plateau.obtenirCase(p);
+            if (c == nullptr) {
+                break;
+            }
+
+            if (!c->estOccupee()) {
+                mouvements.push_back(p);
+            } else {
+                if (c->contientPieceAdverse(couleur)) {
+                    mouvements.push_back(p);
+                }
+                break;
+            }
+
+            ligne += dl;
+            colonne += dc;
+        }
+    }
+
+    return mouvements;
+}
